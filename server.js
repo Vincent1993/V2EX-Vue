ceb78f890af10c10f4a4d916f1ecd2d9eb71a374
Vincent1993 <vincentwang.720@gmail.com>
2016-01-18 16:11:43 +0800

    修改发布配置

--- server.js
+++ server.js
@@ -3,8 +3,8 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
-config.entry.unshift('webpack-dev-server/client?http://localhost:8890',"webpack/hot/dev-server");
-config.plugins.push(new webpack.HotModuleReplacementPlugin());
+// config.entry.unshift('webpack-dev-server/client?http://localhost:8890',"webpack/hot/dev-server");
+// config.plugins.push(new webpack.HotModuleReplacementPlugin());
 
 var proxy = [{
     path: "/api/*",
