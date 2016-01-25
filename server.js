8873f1e7c2243f35b2153819c181e66378e097ca
Vincent1993 <vincentwang.720@gmail.com>
2016-01-25 20:14:40 +0800

    2016-1-25

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
-config.entry.unshift('webpack-dev-server/client?http://localhost:8890',"webpack/hot/dev-server");
+// config.entry.unshift('webpack-dev-server/client?http://localhost:8890',"webpack/hot/dev-server");
 config.plugins.push(new webpack.HotModuleReplacementPlugin());
 config.plugins.push(new webpack.optimize.UglifyJsPlugin({
         compress: {
