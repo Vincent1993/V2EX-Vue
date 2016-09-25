280027879de3d34776f926a5c5adda2aec6c6ef9
Vincent1993 <wangjs@dxyer.com>
2016-09-25 11:19:44 +0800

    增加全局通用配置

---
+++ src/config/index.js
@@ -0,0 +1,30 @@
+/*
+ * @Author: Vincent1993
+ * @Date:   2016-09-24 14:33:56
+ * @Last Modified by:   Vincent1993
+ * @Last Modified time: 2016-09-24 23:25:41
+ */
+
+export const API = 'https://www.v2ex.com/api';
+
+export const TOP_NAV_LIST = [{
+    key: 'latest',
+    value: '最新',
+    icon: 'clock-o',
+    link: 'list'
+}, {
+    key: 'hot',
+    value: '最热',
+    icon: 'fire',
+    link: 'list'
+}, {
+    key: 'node',
+    value: '节点',
+    icon: 'folder',
+    link: 'nodelist'
+}, {
+    key: 'me',
+    value: '我的',
+    icon: 'user',
+    link: 'user'
+}];
